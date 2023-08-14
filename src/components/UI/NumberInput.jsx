2742ffe8cedaf4eef7ff1459a33b6e692b59dd15
VanyaShumilo1 <vanya.shumilo@gmail.com>
2023-08-14 13:16:47 +0300

    add to cart

--- src/components/UI/NumberInput.jsx
+++ src/components/UI/NumberInput.jsx
@@ -1,10 +1,9 @@
-import React, {useState} from 'react';
 import styles from '../../styles/numberInput.module.scss'
 
 const NumberInput = ({value, setValue, ...props}) => {
 
     return (
-        <input className={styles.numberInput} type="text" value={value} onChange={(e) => setValue(e.target.value)}/>
+        <input {...props} className={styles.numberInput} type="text" value={value} onChange={(e) => setValue(e.target.value)}/>
     );
 };
 
