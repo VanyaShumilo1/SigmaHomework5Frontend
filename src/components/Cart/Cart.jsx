83e3988ddd7788871c0f29aa0d6ecaba16efa9f4
VanyaShumilo1 <vanya.shumilo@gmail.com>
2023-08-16 01:18:09 +0300

    form start

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,6 +6,8 @@ import CartItem from "./CartItem";
 import Button from "../UI/Button";
 import buttonStyles from '../../styles/button.module.scss'
 import Input from "../UI/Input";
+import inputStyles from '../../styles/input.module.scss'
+import {Navigate} from "react-router-dom";
 
 const Cart = () => {
 
@@ -27,7 +29,57 @@ const Cart = () => {
 
     const [isFormActive, setIsFormActive] = useState(false)
 
+    const handleShowForm = () => {
+        setIsFormActive(true)
+    }
+
     const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [address, setAddress] = useState('')
+    const [phone, setPhone] = useState('')
+    const [message, setMessage] = useState('')
+
+    const [nameError, setNameError] = useState(false)
+    const [emailError, setEmailError] = useState(false)
+    const [addressError, setAddressError] = useState(false)
+    const [phoneError, setPhoneError] = useState(false)
+
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    const phoneRegex = /\d+/
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        if (name.length >= 3 && emailRegex.test(email) && phoneRegex.test(phone) && phoneRegex.length > 5 && address.length >= 3) {
+
+        }
+
+        if (name.length < 3) {
+            setNameError(true)
+        } else {
+            setNameError(false)
+        }
+
+        if (!emailRegex.test(email)) {
+            setEmailError(true)
+        } else {
+            setEmailError(false)
+        }
+
+        if (address.length < 3) {
+            setAddressError(true)
+        } else {
+            setAddressError(false)
+        }
+
+        if (!phoneRegex.test(phone) || phoneRegex.length <= 5) {
+            setPhoneError(true)
+        } else {
+            setPhoneError(false)
+        }
+    }
+
+    if (cartItems.length === 0) {
+        return <Navigate to={'/'}/>
+    }
 
     return (
         <div className={styles.cart}>
@@ -46,14 +98,46 @@ const Cart = () => {
                     </div>
                 </div>
 
-            <Button className={[buttonStyles.button_blue, styles.cart__button].join(' ')}>To order</Button>
-                <form>
-                    <div>
-                        <label htmlFor="name">Name</label>
-                        <Input id={'name'} value={name} setValue={setName} placeholder={'name'}/>
+                <form className={[styles.cart__form, !isFormActive && styles.hide].join(' ')}>
+                    <div className={styles.cart__form_wrapper}>
+                        <div>
+                            <label className={styles.cart__form_label} htmlFor="name">Full name*</label>
+                            <Input className={[nameError && styles.error].join(' ')} type={'text'} id={'name'}
+                                   value={name} setValue={setName}
+                                   placeholder={'Your Email Address'}/>
+                        </div>
+                        <div>
+                            <label className={styles.cart__form_label} htmlFor="email">Your email*</label>
+                            <Input className={[emailError && styles.error].join(' ')} type={'email'} id={'email'}
+                                   value={email} setValue={setEmail}
+                                   placeholder={'example@yourmail.com'}/>
+                        </div>
+                        <div>
+                            <label className={styles.cart__form_label} htmlFor="address">Address*</label>
+                            <Input className={[addressError && styles.error].join(' ')} type={'text'} id={'address'} value={address} setValue={setAddress}
+                                   placeholder={'Your Company Address'}/>
+                        </div>
+                        <div>
+                            <label className={styles.cart__form_label} htmlFor="phone">Phone number*</label>
+                            <Input className={[phoneError && styles.error].join(' ')} type={'text'} id={'phone'} value={phone} setValue={setPhone}
+                                   placeholder={'Enter Your Phone'}/>
+                        </div>
+                        <div className={styles.cart__form_textAreaWrapper}>
+                            <label className={styles.cart__form_label} htmlFor="message">Message</label>
+                            <textarea className={inputStyles.input} id="message" value={message}
+                                      onChange={(e) => setMessage(e.target.value)}
+                                      placeholder={'Some Extra Information'}></textarea>
+                        </div>
                     </div>
-                </form>
 
+                    <Button className={[buttonStyles.button_blue, styles.cart__form_submit].join(' ')}
+                            onClick={(event) => handleSubmit(event)}>Confirm</Button>
+                </form>
+                <Button
+                    className={[buttonStyles.button_blue, styles.cart__button, isFormActive && styles.hide].join(' ')}
+                    onClick={handleShowForm}>
+                    To order
+                </Button>
             </Container>
         </div>
     );
